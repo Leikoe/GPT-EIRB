c5b88797447ee9e6a7ea1c13605b4c9c63475bdf
Léo Paillé <leo.paille@enseirb-matmeca.fr>
2023-12-13 17:03:16 +0100

    feat: added sampling

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 import pickle
 import time
 from typing import Self, List
+
+import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 
 from tokenizer import tokenizer
@@ -10,6 +12,7 @@ class GPTeirb:
     tokenizer = tokenizer
 
     def __init__(self, train_ncd: List[List[float]], Y: List[int]):
+        return  # TODO: remove this when we have a model to run
         self.neigh = KNeighborsClassifier(n_neighbors=7)
         self.neigh.fit(train_ncd, Y)
 
@@ -22,7 +25,10 @@ class GPTeirb:
         with open(path, "wb") as f:
             pickle.dump(self, f)
 
-    def __call__(self, tokens: List[int], *args, **kwargs) -> int:
+    def __call__(self, tokens: List[int], *args, **kwargs) -> np.ndarray[np.float32]:
         # TODO: implement forward
         time.sleep(0.01)
-        return tokenizer.encode("owo")[1]
+
+        z = np.zeros(self.tokenizer.vocab_size(), dtype=np.float32)
+        z[tokenizer.encode("owo")[1]] = 1.0
+        return z
